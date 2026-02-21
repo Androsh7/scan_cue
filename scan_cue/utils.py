3fdabbc107555baccedfab9ee1afb307e561d955
androsh7@gmail.com <androsh7@gmail.com>
2026-02-21 07:51:35 -0500

    Removed JSON merge function and tests

--- scan_cue/utils.py
+++ scan_cue/utils.py
@@ -7,32 +7,6 @@ from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.primitives.asymmetric import rsa
 
-# Third-party libraries
-# Project libraries
-from scan_cue.ui import ScannerUI
-
-
-def merge_json_files(ui: ScannerUI, files_to_merge: list[Path], output_file: Path):
-    """Merges json files
-
-    Args:
-        ui: The ui object
-        files_to_merge: The json files to merge
-        output_file: The output file
-    """
-    ui.show_progress_bar()
-    ui.update_progress_bar(current_progress=0, total_progress=len(files_to_merge), description="Joining JSON files")
-    with open(file=output_file, mode="wb") as combined_file:
-        combined_file.write(b"[")
-        for index, file_path in enumerate(files_to_merge, start=1):
-            ui.advance_progress_bar()
-            with open(file=file_path, mode="rb") as result_file:
-                combined_file.write(result_file.read().replace(b"\r\n", b"")[1:-1])
-            if index < len(files_to_merge):
-                combined_file.write(b",")
-        combined_file.write(b"]")
-    ui.hide_progress_bar()
-
 
 def create_ssh_key_pair(output_dir: Path) -> tuple[Path, Path]:
     """Creates an ssh key pair
